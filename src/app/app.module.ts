0b1fdae44e2fab63e221aae13cf32a7df1e9550f
Lennin Cobos <ivancho3110@hotmail.com>
2019-08-02 02:22:57 -0500

    Login Admi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { PrivadoComponent } from './componentes/privado/privado.component';
+
+import {AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { environment } from '../environments/environment';
+import { AuthService } from './servicios/auth.service';
+import { from } from 'rxjs';
+import { AuthGuard } from './guards/auth.guard';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    PrivadoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard, FlashMessagesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB9gO8Bt4UTjm-sI_kY_7wjp7l4RxW_HYM",
+    authDomain: "cuchitril-77f74.firebaseapp.com",
+    databaseURL: "https://cuchitril-77f74.firebaseio.com",
+    projectId: "cuchitril-77f74",
+    storageBucket: "cuchitril-77f74.appspot.com",
+    messagingSenderId: "584114981456",
+    appId: "1:584114981456:web:9decd7f0de315d0a"
+  }
 };
 
 /*
